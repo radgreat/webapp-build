120fe895f6a7351b319048d5ab08c9755fdc9109
rad <radgreat@gmail.com>
2026-04-02 00:43:48 +0800

    param on env

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,13 +4,17 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const { Pool } = pg
+const isProductionDb = process.env.DB_HOST && process.env.DB_HOST.includes('render.com')
 
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: isProductionDb
+    ? { rejectUnauthorized: false }
+    : false
 })
 
 export default pool
